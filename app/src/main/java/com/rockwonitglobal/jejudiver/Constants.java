253e14502f7232c4976d6b7fadede12ff3e43726
bmyi <bmyi@rockwonitglobal.com>
2022-05-12 10:01:05 +0900

    app folder -

--- app/src/main/java/com/rockwonitglobal/jejudiver/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.rockwonitglobal.jejudiver;
-
-public class Constants {
-
-    public static final String BASE_URL = "http://220.118.147.50:9070/" ;
-    public static String SET_DATA = "apiDevice/setData";
-}
